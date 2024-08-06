d94691936e88ff52bfc55fb8bf1d3278a21893c1
Stephon Fraser <programmer3@muneshwers.com>
2024-08-06 13:08:57 -0400

    Added footer, careers style page

--- src/routes/careers/+page.svelte
+++ src/routes/careers/+page.svelte
@@ -1,4 +1,6 @@
 <script>
+// @ts-nocheck
+
     import mllogo from "../../logo.png";
     import heroImage from "../../careers.jpg";
     import grid from "../../grid.svg";
@@ -22,9 +24,11 @@
     import store2Service from "../../events1.jpg";
 	import Joint from "../Joint.svelte";
 	import Navigation from "../Navigation.svelte";
+	import Footer from "../Footer.svelte";
 
     $: jvToggle = false;
 
+    let y;
 </script>
 
 <svelte:head>
@@ -49,43 +53,18 @@
     <Navigation activePage="careers"/>
 </div>
 <div class="introduction-section flex flex-col justify-center">
-    <div class="intro-header mt-7 justify-center relative">
-        <img src={grid} alt="mesh grid" class="h-auto w-full">
-        <div class="intro-details flex items-center absolute top-16 bottom-16 left-16 w-11/12 justify-between">
-            <div class="intro-icon-box flex justify-center items-center">
-                <div class="intro-icon"></div>
-            </div>
-            <div class="intro-details w-3/4">
-                <div class="intro-text text-right w-[1000px] uppercase flex flex-col text-[76px] text-[#4876B6]">
-                    <div class="text-right flex flex-row-reverse w-full">
-                        Choose the career that's right for you!
-                    </div>
+    <div class="intro-header my-12 justify-center relative flex h-[143px]">
+            <div class="intro-details">
+                <div class="intro-text mt-8 text-center uppercase flex flex-col text-[50px] text-[#4876B6]">
+                    <p>Choose the career that's right for you!</p>
                 </div>
-                <div class="intro-subtext text-right text-[#3D3938] text-[28px] raleway-light">
-                    Choose from our list of available job opportunities below.
+                <div class="intro-subtext text-center text-[#3D3938] text-[28px] raleway-light flex flex-col items-center">
+                    <p>Choose from our list of available job opportunities below.</p>
                 </div>
             </div>
-            
-        </div>
     </div>
-    <div class="services flex justify-center items-center mb-7 mt-10 w-full">
-        <div class="flex flex-row gap-8 relative h-[120vh] w-9/12">
-            <div class="service-container w-[250px] h-[700px] absolute">
-                <img alt="" class="w-[250px] h-[700px] rounded-2xl object-cover cursor-pointer" src={shippingService}  />
-            </div>
-            <div class="service-container-low w-[250px] h-[700px] absolute left-[26.7%] top-44">
-                <img alt="" class="w-[250px] h-[700px] rounded-2xl object-cover cursor-pointer" src={travelService} />
-            </div>
-            <div class="service-container w-[250px] h-[700px] absolute right-[26.7%] top-10">
-                <img alt="" class="w-[250px] h-[700px] rounded-2xl  object-cover cursor-pointer" src={store1Service} />
-            </div>
-            <div class="service-container-low w-[250px] h-[700px] absolute right-0 top-20">
-                <img alt="" class="w-full h-full rounded-2xl left-full  object-cover cursor-pointer" src={store2Service} />
-            </div>
-            <!-- <div class="service-container w-[250px] h-[700px]">
-                <img alt="" class="w-full h-full rounded-2xl left-5/5 " src="https://via.placeholder.com/250x700" />
-            </div> -->
-        </div>
+    <div class="benefits">
+        
     </div>
 </div>
 <div class="parallax-container relative">
@@ -168,60 +147,7 @@
         </div>
     </div>
 </div>
-<div class="footer h-[450px] bg-[#EDEBE5]">
-    <div class="footer-top flex flex-row gap-4 p-10">
-        <div class="site-info w-1/5">
-            <div class="site-name flex flex-row w-full">
-                <img src={brandLogo} alt="Muneshwers Shipping" class="object-cover w-[150px] h-[100px]">
-                <img src={textLogo} alt="Muneshwers Shipping" class="object-cover w-[200px] h-[100px]">
-            </div>
-            <div class="site-tagline playfair-display-regular text-[18px] ml-4 mt-2">
-                Offering solutions, not excuses...
-            </div>
-            <div class="social-media h-2/3 w-full flex flex-col gap-4 mt-7 raleway-light" >
-                <a href="https://www.facebook.com/profile.php?id=100087564355386" target="_blank" class="social-link flex flex-row items-center gap-2">
-                    <img src={facebookIcon} alt="Muneshwers Shipping" class="object-cover w-[30px] h-[30px]">
-                    <div class="social-name">Muneshwers Shipping</div>
-                </a>
-                <a href="https://www.instagram.com/muneshwersshipping/" target="_blank" class="social-link flex flex-row items-center gap-2">
-                    <img src={instagramIcon} alt="Muneshwers Shipping" class="object-cover w-[30px] h-[30px]">
-                    <div class="social-name">@muneshwersshipping</div>
-                </a>
-                <a href="https://www.tiktok.com/@muneshwersshipping/" target="_blank" class="social-link flex flex-row items-center gap-2 ">
-                    <img src={tiktokIcon} alt="Muneshwers Shipping" class="object-cover w-[30px] h-[30px]">
-                    <div class="social-name">@muneshwersshipping</div>
-                </a>
-                <a href="https://www.linkedin.com/company/muneshwers-limited/" target="_blank" class="social-link flex flex-row items-center gap-2">
-                    <img src={linkedinIcon} alt="Muneshwers Shipping" class="object-cover w-[30px] h-[30px]">
-                    <div class="social-name">Muneshwers Limited</div>
-                </a>
-            </div>
-        </div>
-        <div class="company-info">
-            <div class="footer-section w-2/3">
-                <div class="footer-section-title poppins-medium text-2xl">
-                    Address
-                </div>
-                <div class="footer-section-info poppins-light mt-4">
-                    45-47 Water Street, Georgetown, Guyana
-                </div>
-            </div>
-            <div class="footer-section mt-12 w-2/3">
-                <div class="footer-section-title poppins-medium text-2xl">
-                    Contact Info
-                </div>
-                <div class="footer-section-info poppins-light mt-4 flex flex-col gap-4">
-                    <div class="info-number">
-                        +592-227-7417/18
-                    </div>
-                    <div class="info-email">
-                        info@muneshwers.com
-                    </div>
-                </div>
-            </div>
-        </div>
-        
-    </div>
-    <div class="footer-bottom"></div>
-</div>
+<Footer />
+
+<svelte:window bind:scrollY={y} />
 
