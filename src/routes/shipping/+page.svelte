7bdf9fb0171414e6b83813e1a7d278543410db5a
Stephon Fraser <stephonfraserofficial@gmail.com>
2024-09-13 10:02:09 -0400

    Added images for shipping bento

--- src/routes/shipping/+page.svelte
+++ src/routes/shipping/+page.svelte
@@ -2,17 +2,27 @@
 // @ts-nocheck
 
     import brandLogo from "$lib/assets/brand-logo.webp";
+    import mllogo from "$lib/assets/logo.png";
 	import Navigation from "../Navigation.svelte";
 	import Footer from "../Footer.svelte";
-    import ship from "$lib/assets/ship-solid.svg";
-    import anchor from "$lib/assets/anchor-solid.svg";
-    import truck from "$lib/assets/truck-solid.svg";
-    import truckRamp from "$lib/assets/truck-ramp-box-solid.svg";
-    import warehouse from "$lib/assets/warehouse-solid.svg";
-    import dolly from "$lib/assets/dolly-solid.svg";
-    import oil from "$lib/assets/oil-well-solid.svg";
-    import box from "$lib/assets/box-solid.svg";
-    import boxOpen from "$lib/assets/box-open-solid.svg";
+    import ship from "$lib/assets/shippingagency.jpg";
+    import anchor from "$lib/assets/vesselhandling.jpg";
+    import truck from "$lib/assets/GYS09655.jpg";
+    import truckRamp from "$lib/assets/dangerouscargo.jpg";
+    import warehouse from "$lib/assets/warehousing.png";
+    import dolly from "$lib/assets/oilandgassupport.jpg";
+    import oil from "$lib/assets/oilandgassupport.jpg";
+    import box from "$lib/assets/customsbrokerage.jpg";
+    import boxOpen from "$lib/assets/container2.jpg";
+    // import ship from "$lib/assets/ship-solid.svg";
+    // import anchor from "$lib/assets/anchor-solid.svg";
+    // import truck from "$lib/assets/truck-solid.svg";
+    // import truckRamp from "$lib/assets/truck-ramp-box-solid.svg";
+    // import warehouse from "$lib/assets/warehouse-solid.svg";
+    // import dolly from "$lib/assets/dolly-solid.svg";
+    // import oil from "$lib/assets/oil-well-solid.svg";
+    // import box from "$lib/assets/box-solid.svg";
+    // import boxOpen from "$lib/assets/box-open-solid.svg";
 
     let y;
 </script>
@@ -28,7 +38,7 @@
         <div class="image-cover h-full w-full opacity-30 bg-[#3A3A3A] absolute top-0"></div>
         <div class="hero-text uppercase text-white absolute bottom-48  left-16 w-9/12 leading-[120px] flex flex-col">
             <div class="individual-hero-text logo w-36 h-36">
-                <img src={brandLogo} alt="Muneshwers Limited Logo" class="h-full w-full">
+                <img src={mllogo} alt="Muneshwers Limited Logo" class="h-full w-full">
             </div>
             <div>
                 <p class="individual-hero-text text-[60px]">Muneshwers</p>
@@ -53,13 +63,39 @@
         <div class="intro-details">
             {#if y > 200}
             <div class="intro-text mt-8 text-center uppercase flex flex-col text-[50px] text-[#4876B6]">
-                <p>Ship with the Fastest growing port in Guyana</p>
+                <p>Our Services</p>
             </div>
             {/if}
             
         </div>
     </div>
-    <div class="services shipping-image w-[100%] ">
+    <div class="services shipping-image w-[95%] flex items-center justify-center">
+         {#if y > 300}
+         <div class="grid-container mt-5 ">
+            <div class="grid grid-cols-4 gap-2 h-[100%]">
+                <div class="service h-full w-full  rounded-md">
+                    <img src={ship} alt="Hero landing page" class=" w-full h-full object-cover" />
+                </div>
+                <div class="service h-full w-full rounded-md">
+                    <img src={anchor} alt="Hero landing page" class="w-full h-full object-cover" />
+                </div>
+                <div class="service h-full w-full rounded-md row-span-2 col-span-2">
+                    <img src={truck} alt="Hero landing page" class="w-full h-full object-cover" />
+                </div>
+                <div class="service h-full w-full rounded-md col-span-2">
+                    <img src={warehouse} alt="Hero landing page" class="w-full h-full object-cover" />
+                </div>
+                <div class="service h-full w-full rounded-md">
+                    <img src={box} alt="Hero landing page" class="w-full h-full object-cover" />
+                </div>
+                <div class="service h-full w-full rounded-md  col-span-3">
+                    <img src={boxOpen} alt="Hero landing page" class="w-full h-full object-cover" />
+                </div>
+            </div>
+         </div>
+        {/if}
+    </div>
+    <!-- <div class="services shipping-image w-[100%] ">
          {#if y > 300}
          <div class="list-row mt-5 flex flex-row justify-center gap-5">
             <div class="service w-[25%] h-[139px] bg-white rounded-[10px] shadow flex flex-row items-center gap-8">
@@ -122,7 +158,7 @@
             </div>
         </div>
         {/if}
-    </div>
+    </div> -->
  
 </div>
 <div class="entrance h-[70vh] my-36 flex justify-center items-center gap-10">
