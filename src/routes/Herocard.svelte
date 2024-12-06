ad00afb968bb77711785b553f758b522bd56ed91
Stephon Fraser <stephonfraserofficial@gmail.com>
2024-12-06 14:25:57 -0400

    Added slideshow for hero image

---
+++ src/routes/Herocard.svelte
@@ -0,0 +1,19 @@
+<script>
+    import shipping from "$lib/assets/GYS00823.webp";
+    import stores from "$lib/assets/IMG_4167_MOD.webp";
+
+    export let slideImage;
+
+    let slideChoice;
+
+    console.log("Slide info: ", slideImage.slideId)
+
+    if(slideImage.slideId == 1) {
+        slideChoice = shipping;
+    } else {
+        slideChoice = stores
+    }
+
+</script>
+
+<img class="object-cover w-full h-screen" src={slideChoice} alt="Hero of staff" />
